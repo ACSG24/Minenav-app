2d1985a4a962918f4620cc5528541b3dafad55e0
ACSG24 <acsg2424@gmail.com>
2025-06-06 21:13:50 -0400

    Fix manifest.json, sw.js and add 404.html

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'minenav-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  // Forzar la activación del SW inmediatamente
+  self.skipWaiting();
+});
+
+// Activar Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Tomar control de todas las páginas inmediatamente
+  self.clients.claim();
+});
+
+// Interceptar requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(() => {
+        // Si falla todo, devuelve index.html
+        return caches.match('./index.html');
+      })
+  );
+});
